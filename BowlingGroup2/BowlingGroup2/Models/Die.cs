4ec6759ad1543ba58c516d45ffe13f33eb729bd5
Matthew Price <matthew.price@microlise.com>
2019-01-28 13:48:41 +0000

    Added static DIe class

---
+++ BowlingGroup2/BowlingGroup2/Models/Die.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGroup2.Models
+{
+    public static class Die
+    {
+        public static Random _ran = new Random();
+        public static int GetNextBowl(int remainPinCount)
+        {
+            //To Account for the exclusive upper bound
+            return _ran.Next(0, remainPinCount+1);
+        }
+    }
+}
