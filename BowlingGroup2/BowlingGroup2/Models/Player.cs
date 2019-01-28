dbdc38c9500a0f8590f2fbc59ce974af6d8ce03c
Matthew Price <matthew.price@microlise.com>
2019-01-28 14:10:39 +0000

    added player class

---
+++ BowlingGroup2/BowlingGroup2/Models/Player.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingGroup2.Models
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public List<int> Scores = new List<int>();
+
+        public Player(string name)
+        {
+            this.Name = name;
+        }
+
+        public void AddScore(int roundScore)
+        {
+            Scores.Add(roundScore);
+        }
+
+    }
+}
