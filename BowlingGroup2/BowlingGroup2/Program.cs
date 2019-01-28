27781638afeced19a3ad9f61136560a9d15c392f
Eamon Pryme <eamon.pryme@microlise.com>
2019-01-28 14:11:31 +0000

    Catch up from earlier git error

--- BowlingGroup2/BowlingGroup2/Program.cs
+++ BowlingGroup2/BowlingGroup2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using BowlingGroup2.Models;
 
 namespace BowlingGroup2
 {
@@ -17,6 +18,23 @@ namespace BowlingGroup2
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new SetUpView());
+
+            for (int i = 0; i < 10; i++)
+            {
+                Player.addScore(i, Frame());
+            }
+
+        }
+
+        static int Frame()
+        {
+            int firstBowl = 0;
+            int secondBowl = 0;
+
+            firstBowl = Die.GetNextBowl(10);
+            secondBowl = Die.GetNextBowl(10-firstBowl);
+
+            return secondBowl + firstBowl;
         }
     }
 }
